9cbc619c98a447a954389a3da7cf38ff06d84eae
Scott Warchal <s.warchal@sms.ed.ac.uk>
2018-07-06 10:13:25 +0100

    update 2018-07-06

--- count_words.sh
+++ count_words.sh
@@ -5,5 +5,5 @@ MASTER_DOC=/home/scott/Dropbox/thesis/sw_thesis.tex
 DATE=`date '+%Y-%m-%d'`
 WORDCOUNT=$(/usr/bin/texcount "$MASTER_DOC" -inc -total | grep "Words in text" |cut -d ' ' -f 4)
 echo -e "$DATE""\t""$WORDCOUNT" >> /home/scott/Dropbox/thesis/word_count.tsv
-python /home/scott/Dropbox/thesis/plot_word_count.py
+python plot_word_count.py
 exit 0
